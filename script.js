12871723212bbdc0990b210302049475c1379742
Julius <188955756+4483-s@users.noreply.github.com>
2025-03-29 12:55:28 +0800

    rewrite functions

--- script.js
+++ script.js
@@ -3,55 +3,27 @@ const btns = document.querySelectorAll("button");
 //DOM
 display.textContent = "0";
 let operand1 = "";
+let operator = "";
 let operand2 = "";
-let operator = null;
 
 btns.forEach((btn) => {
   btn.addEventListener("click", function () {
-    act(btn.textContent);
+    if (btn.textContent.match(/\d/)) {
+      digit(btn.textContent);
+    } else {
+      fn(btn.textContent);
+    }
   });
 });
-
-function act(content) {
-  if (content === "AC") {
-    operand1 = "";
-    operand2 = "";
-    operator = null;
-    display.textContent = "0";
-    return;
+function digit(content) {
+  if (!operator) {
+    operand1 += content;
+  } else {
+    operand2 += content;
   }
-  operator =
-    content === "/"
-      ? "/"
-      : content === "*"
-      ? "*"
-      : content === "-"
-      ? "-"
-      : content === "+"
-      ? "+"
-      : null;
-  if (content.match(/\d/)) {
-    if (!operator) {
-      operand1 += content;
-    } else {
-      operand2 += content;
-    }
+  display.textContent = operand1;
+}
+function fn(content) {
+  if (content === "=") {
   }
-  if (operator === "*") {
-    operand1 = +operand1 * +operand2;
-    operator = null;
-  }
-  if (operator === "+") {
-    operand1 = +operand1 + +operand2;
-    operator = null;
-  }
-  if (operator === "-") {
-    operand1 = +operand1 - +operand2;
-    operator = null;
-  }
-  if (operator === "/") {
-    operand1 = +operand1 / +operand2;
-    operator = null;
-  }
-  display.textContent = operator ? operand1 + operator : operand1;
 }
