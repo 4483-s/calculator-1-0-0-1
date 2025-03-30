432b917ffdbbee975afdf3c79fdc698b7a8d9f08
Julius <188955756+4483-s@users.noreply.github.com>
2025-03-30 23:13:07 +0800

    rewrite functions

--- script.js
+++ script.js
@@ -2,28 +2,60 @@ const display = document.querySelector(".display >p");
 const btns = document.querySelectorAll("button");
 //DOM
 display.textContent = "0";
-let operand1 = "";
-let operator = "";
-let operand2 = "";
-
+let operation = "0";
 btns.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    if (btn.textContent.match(/\d/)) {
-      digit(btn.textContent);
+  btn.addEventListener("click", function (e) {
+    if (e.target.textContent.match(/[.\d]/)) {
+      getDigit(e.target.textContent);
     } else {
-      fn(btn.textContent);
+      fn(e.target.textContent);
     }
   });
 });
-function digit(content) {
-  if (!operator) {
-    operand1 += content;
+const re = new RegExp(/[-/*+]/);
+function getDigit(content) {
+  if (operation === "0") {
+    operation = content;
   } else {
-    operand2 += content;
+    if (operation.includes(".") && content === ".") {
+      return;
+    } else {
+      operation += content;
+    }
   }
-  display.textContent = operand1;
+  display.textContent = operation;
 }
 function fn(content) {
-  if (content === "=") {
+  // console.log(content);
+  const operationSplited = operation.trim().split(" ");
+  opLength = operationSplited.length;
+  //AC reaction
+  if (content === "AC") {
+    operation = "0";
+    display.textContent = operation;
   }
+  // minus
+  else if (content.match(re)) {
+    if (operationSplited[opLength - 1].match(re)) {
+      operation = operation.replace(operation.at(-2), content);
+    }
+    //
+    else if (opLength === 3) {
+      if (operationSplited[1] === "-") {
+        operation = operationSplited[0] - operationSplited[2] + ` ${content} `;
+      } else if (operationSplited[1] === "+") {
+        operation =
+          +operationSplited[0] + +operationSplited[2] + ` ${content} `;
+      } else if (operationSplited[1] === "/") {
+        operation = operationSplited[0] / operationSplited[2] + ` ${content} `;
+      } else {
+        operation = operationSplited[0] * operationSplited[2] + ` ${content} `;
+      }
+    } else {
+      operation += ` ${content} `;
+    }
+    display.textContent = operation;
+  }
+
+  console.log(operation);
 }
